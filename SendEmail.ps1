8b0664a7bd5d0334c7c9170dfffb9e6d0b0afb62
Umar Khan <umarkhan252@gmail.com>
2019-08-19 09:34:42 -0700

    email script

---
+++ SendEmail.ps1
@@ -0,0 +1,12 @@
+﻿$EmailFrom = “sender”
+$EmailTo = "recepient"
+$Subject = “”
+$Body = “”
+$SMTPServer = “”
+$Port = 587;
+$Username = ""
+$Password = ""
+$SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer, $Port)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($Username,$Password);
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
