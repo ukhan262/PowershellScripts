f9b13d97b60a6169f7dec15c0c01eb6da29e8a7d
ukhan262 <37010344+ukhan262@users.noreply.github.com>
2019-10-08 15:27:52 -0700

    removed exact folder

--- RemoveEmptyFolders.ps1
+++ RemoveEmptyFolders.ps1
@@ -1,3 +1,3 @@
-﻿$tdc="C:\AccountingRequestDownload\"
+﻿$tdc="foldername"
 $dirs = gci $tdc -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
 $dirs | Foreach-Object { Remove-Item $_ }
