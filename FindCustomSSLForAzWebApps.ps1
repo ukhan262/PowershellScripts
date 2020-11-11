b6252dc862ccc35c932e3e236f5c14e58a70b60a
ukhan262 <37010344+ukhan262@users.noreply.github.com>
2020-11-11 01:30:46 -0800

    Update FindCustomSSLForAzWebApps.ps1

--- FindCustomSSLForAzWebApps.ps1
+++ FindCustomSSLForAzWebApps.ps1
@@ -4,7 +4,7 @@ Import-Module -Name Az
 
 $RGs = @()
 
-$Subscriptions = '118247e6-80ef-4a95-a9cc-3c656affb9ea'
+$Subscriptions = 'xxxxxxxxxxxxxxxxxxxxxx'
 Select-AzSubscription -SubscriptionId $Subscriptions
 $RGs += Get-AzResourceGroup
     
@@ -12,6 +12,6 @@ $RGs += Get-AzResourceGroup
 foreach($rg in $RGs) {
         $WAs = Get-AzWebApp -ResourceGroupName $rg.ResourceGroupName -ErrorAction SilentlyContinue
         foreach ($wa in $WAs) {
-            Get-AzWebAppSSLBinding -ResourceGroupName $rg.ResourceGroupName -WebAppName $wa.Name | Where-Object {$_.Name -like "wedgewood-inc.com"} -ErrorAction SilentlyContinue
+            Get-AzWebAppSSLBinding -ResourceGroupName $rg.ResourceGroupName -WebAppName $wa.Name | Where-Object {$_.Name -like '*wedgewood-inc.com*'} -ErrorAction SilentlyContinue
         }     
     }
