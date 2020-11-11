fb4883973b49b9d6815427dc17083ecdeb0f2393
ukhan262 <37010344+ukhan262@users.noreply.github.com>
2020-11-11 01:43:15 -0800

    Update FindCustomSSLForAzWebApps.ps1

--- FindCustomSSLForAzWebApps.ps1
+++ FindCustomSSLForAzWebApps.ps1
@@ -2,20 +2,23 @@ $ErrorActionPreference = "SilentlyContinue"
 
 Import-Module -Name Az
 
-$RGs = @()
+
 $WAssl = @()
 
-$Subscriptions = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-Select-AzSubscription -SubscriptionId $Subscriptions
-$RGs += Get-AzResourceGroup
+$Subscriptions = Get-AzSubscription
+foreach ($sub in $Subscriptions)
+{
+    $RGs = @()
+    Select-AzSubscription -SubscriptionId $sub.Id
+    $RGs += Get-AzResourceGroup
     
 
-foreach($rg in $RGs) {
-    $WAs = Get-AzWebApp -ResourceGroupName $rg.ResourceGroupName
-    foreach ($wa in $WAs) {
-        $WAssl += Get-AzWebAppSSLBinding -ResourceGroupName $rg.ResourceGroupName -WebAppName $wa.Name `
-            | Where-Object {$_.Name -like '*wedgewood-inc.com*'}
-    }     
+    foreach($rg in $RGs) {
+        $WAs = Get-AzWebApp -ResourceGroupName $rg.ResourceGroupName
+        foreach ($wa in $WAs) {
+            $WAssl += Get-AzWebAppSSLBinding -ResourceGroupName $rg.ResourceGroupName -WebAppName $wa.Name `
+               # | Where-Object {$_.Name -like '*wedgewood-inc.com*'}
+        }     
+    }
 }
-
 $WAssl | ft
