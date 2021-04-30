520716265c807b09f36c52a2414e51f6e6585b17
Umar Khan <Umar.Khan@cudirect.com>
2021-04-30 06:05:51 -0700

    code cleanup and standardization

--- PSScriptAnalyzer.ps1
+++ PSScriptAnalyzer.ps1
@@ -7,7 +7,7 @@ foreach ($folder in $psFolders.Name)
   $errors   = $issues.Where({$_.Severity -eq 'Error'})
   $warnings = $issues.Where({$_.Severity -eq 'Warning'})
   if ($errors) {
-      Write-Error "There were $($errors.Count) errors and $($warnings.Count) warnings total." -ErrorAction Continue
+      Write-Error "There were $($errors.Count) errors and $($warnings.Count) warnings total." -ErrorAction SilentlyContinue
   } else {
       Write-Output "There were $($errors.Count) errors and $($warnings.Count) warnings total."
   }
