7222b62a022bcd52011c078b606fd53d0c2a1a6b
Umar Khan <Umar.Khan@cudirect.com>
2021-05-11 16:42:29 -0700

    add standards for ps script

--- Azure Scripts/AzGetNetworkInterfaces.ps1
+++ Azure Scripts/AzGetNetworkInterfaces.ps1
@@ -1,17 +1,17 @@
 ﻿$ErrorActionPreference = "SilentlyContinue"
 Import-Module -Name Az
 $Subscriptions = Get-AzSubscription
-$reportName = "CUDirectVmsInfo.csv"
+$reportName = "VmInfo.csv"
 $report = @()
 
 foreach ($sub in $Subscriptions){
     Select-AzSubscription -SubscriptionId $sub.SubscriptionId
     $vms = Get-AzVM
     $publicIps = Get-AzPublicIpAddress 
-    $nics = Get-AzNetworkInterface | ?{ $_.VirtualMachine -NE $null} 
+    $nics = Get-AzNetworkInterface | Where-Object{ $_.VirtualMachine -NE $null} 
     foreach ($nic in $nics) {
-        $info = "" | Select VmName, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup
-        $vm = $vms | ? -Property Id -eq $nic.VirtualMachine.id 
+        $info = "" | Select-Object VmName, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup
+        $vm = $vms | Where-Object -Property Id -eq $nic.VirtualMachine.id 
         foreach($publicIp in $publicIps) { 
             if($nic.IpConfigurations.id -eq $publicIp.ipconfiguration.Id) {
                 $info.PublicIPAddress = $publicIp.ipaddress
@@ -30,4 +30,4 @@ foreach ($sub in $Subscriptions){
             $report+=$info 
         }
     }
-$report | ft VmName, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup | Out-File "C:\users\umar.khan\Documents\cudirectvms.txt"
+$report | format-table VmName, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup | Out-File .\$reportName
