9cec2f001c92cfcbb637f62017622d49d520dc8d
Umar Khan <Umar.Khan@cudirect.com>
2021-05-20 23:50:34 -0700

    added handling for interactive login

--- Azure Scripts/AzMigrateResources.ps1
+++ Azure Scripts/AzMigrateResources.ps1
@@ -27,30 +27,32 @@
             - Resources are moved
 
     .EXAMPLE
-        .\AzMigrateResources.ps1 -clientId "" -tenantId "" -clientSecret "" -sourceSub "sourceSubName" -sourceResourceGroup "rgName" -resourceType Microsoft.KeyVault/vaults -resourceName "kv-migration-sbx-01" -destinationSub "destSubName" -destinationResourceGroup "destRgName"
+        .\AzMigrateResources.ps1 -sourceSub "NonProd-West" -sourceResourceGroup "rg-ent-eddms-qa2-01" -resourceType Microsoft.KeyVault/vaults -resourceName "kv-enterprise-qa-02" -destinationSub "NonProd-West" -destinationResourceGroup "rg-ent-commonapps-qa2-01"
 
     .EXAMPLE
-        .\AzMigrateResources.ps1 -clientId "" -tenantId "" -clientSecret "" -sourceSub "sourceSubName" -sourceResourceGroup "rgName" -resourceType Microsoft.DocumentDB/databaseAccounts -resourceName "cosdb-migration-sbx-01" -destinationSub "destSubName" -destinationResourceGroup "destRgName"
-    
+        .\AzMigrateResources.ps1 -sourceSub "NonProd-West" -sourceResourceGroup "rg-commonapps-qa2-01" -resourceType Microsoft.DocumentDB/databaseAccounts -resourceName "cosdb-ent-tenant-sql-api-qa02" -destinationSub "NonProd-West" -destinationResourceGroup "rg-ent-commonapps-qa2-01"
+        
+    .EXAMPLE  
+        .\AzMigrateResources.ps1 -sourceSub "NonProd-West" -sourceResourceGroup "rg-ent-edpn-qa2-01" -resourceType Microsoft.ServiceBus/namespaces -resourceName "sb-ent-events-qa2-101" -destinationSub "NonProd-West" -destinationResourceGroup "rg-ent-commonapps-qa2-01"
+        
     .EXAMPLE
-        .\AzMigrateResources.ps1 -clientId "" -tenantId "" -clientSecret "" -sourceSub "NonProd-West" -sourceResourceGroup "rg-ent-eddms-dev-01" -resourceType Microsoft.Storage/storageAccounts -resourceName "stenteddmsdev01" -destinationSub "NonProd-West" -destinationResourceGroup "rg-ent-eddms-dev-01"
-
+        .\AzMigrateResources.ps1 -sourceSub "IAC-West" -sourceResourceGroup "rg-crm-intuvo-test-dev-02" -resourceType Microsoft.Storage/storageAccounts -resourceName "examplesatest123" -destinationSub "IAC-West" -destinationResourceGroup "do-not-delete"
 #>
 
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory)]
+    [Parameter()]
     [string] $clientId = "",
-    [Parameter(Mandatory)]
+    [Parameter()]
     [string] $tenantId = "",
-    [Parameter(Mandatory)]
+    [Parameter()]
     [string] $clientSecretId = "",
     [Parameter(Mandatory)]
     [string] $sourceSub = "",
     [Parameter(Mandatory)]
     [string] $sourceResourceGroup = "",
     [Parameter(Mandatory)]
-    [ValidateSet("Microsoft.Storage/storageAccounts","Microsoft.DocumentDB/databaseAccounts", "Microsoft.KeyVault/vaults")]
+    [ValidateSet("Microsoft.ServiceBus/namespaces", "Microsoft.Storage/storageAccounts","Microsoft.DocumentDB/databaseAccounts", "Microsoft.KeyVault/vaults")]
     [string] $resourceType = "",
     [Parameter(Mandatory)]
     [string] $resourceName = "",
@@ -60,9 +62,22 @@ param (
     [string] $destinationResourceGroup = ""
 )
 
-$clientSecret = ConvertTo-SecureString $clientSecretId -AsPlainText -Force
-$psCred = New-Object System.Management.Automation.PSCredential($clientId , $clientSecret)
-Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal
+[bool] $useInteractiveLogin = $false
+try {
+    $clientSecret = ConvertTo-SecureString $clientSecretId -AsPlainText -Force
+    $psCred = New-Object System.Management.Automation.PSCredential($clientId , $clientSecret)
+    Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal -ErrorAction Stop
+}
+catch {
+    Write-Host "Failed to connect to the tenant using serice principal" -ForegroundColor Red
+    $_.Exception
+    $useInteractiveLogin = $true
+}
+
+if ($useInteractiveLogin)
+{
+    Connect-AzAccount
+}
 
 Select-AzSubscription -Subscription $sourceSub
 
@@ -75,6 +90,7 @@ try
 }
 catch 
 {
+    $_.Exception
     $privateEndpointsExist = $false
     Write-Host "no private endpoints found in the resource group" $sourceResourceGroup -ForegroundColor Yellow
 }
@@ -93,6 +109,7 @@ if ($privateEndpoints)
         }
     }
     catch{
+        $_.Exception
         Write-Host "issues moving the resource" $privateEndpointResourceId -ForegroundColor Red  
     }
 }
@@ -107,6 +124,7 @@ try
 }
 catch 
 {
+    $_.Exception
     $resourceExists = $false
     Write-Host $resourceName "does not exist" -ForegroundColor Red
 }
@@ -123,6 +141,7 @@ if ($resourceExists)
     }
     catch
     {
+        $_.Exception
         Write-Host "issues moving the resource" $resourceName -ForegroundColor Red
     }
     
