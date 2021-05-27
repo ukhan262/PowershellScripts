98a0717546bfb1a421a66bc13e21bb3e71753630
Umar Khan <Umar.Khan@cudirect.com>
2021-05-27 14:23:14 -0700

    modified to connect via service principal

--- Azure Scripts/AzKeyVaultSecretBulkAdd.ps1
+++ Azure Scripts/AzKeyVaultSecretBulkAdd.ps1
@@ -23,6 +23,15 @@
 param (
     [Parameter(mandatory)]
     [string]
+    $tenantId = "",
+    [Parameter(mandatory)]
+    [string]
+    $clientId = "",
+    [Parameter(mandatory)]
+    [string]
+    $clientSecret = "",
+    [Parameter(mandatory)]
+    [string]
     $subscription = "",
     [Parameter(mandatory)]
     [string]
@@ -32,8 +41,12 @@ param (
     $file = ""
     
 )
+Import-Module -Name Az
 
-Connect-AzAccount
+$azurePassword = ConvertTo-SecureString $clientSecret -AsPlainText -Force
+
+$psCred = New-Object System.Management.Automation.PSCredential($clientId, $azurePassword)
+Connect-AzAccount -Credential $psCred -TenantId $tenantId  -ServicePrincipal 
 
 $fileLocation = ".\$file.csv"
 
@@ -43,5 +56,13 @@ ForEach-Object {
     write-host $_.KeyName -ForegroundColor Yellow
     Write-Host $_.SecretValue -ForegroundColor Yellow
     $Secret = ConvertTo-SecureString -String $_.SecretValue -AsPlainText -Force
-    Set-AzKeyVaultSecret -VaultName $keyvaultName -Name $_.KeyName -SecretValue $Secret
+    try{
+        Set-AzKeyVaultSecret -VaultName $keyvaultName -Name $_.KeyName -SecretValue $Secret -ErrorAction Stop
+        Write-Host "Key added successfully" $_.KeyName -ForegroundColor Green
+    }
+    catch{
+        Write-Host "Failed to add" $_.KeyName -ForegroundColor Red
+        $_.ErrorDetails
+    }
+    
 }
