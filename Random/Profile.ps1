203f0d4ac3cd47fd2dd86ae966581c2ef83a457d
Admin Umar Khan <admin.ukhan@cudirect.com>
2022-08-23 22:24:59 -0700

    adding a couple of files random files

--- Random/Profile.ps1
+++ Random/Profile.ps1
@@ -2,6 +2,12 @@
 function New-TFI { terraform init }
 set-Alias -Name "tfi" -Value New-TFI
 
+function New-TFIR { terraform init -reconfigure }
+set-Alias -Name "tfir" -Value New-TFIR
+
+function New-TFIU { terraform init -upgrade }
+set-Alias -Name "tfiu" -Value New-TFIU
+
 function New-TFP { terraform plan }
 set-alias -Name "tfp" -Value New-TFP
 
@@ -11,7 +17,10 @@ set-alias -Name "tfa" -Value New-TFA
 function New-TFAA { terraform apply -auto-approve }
 set-alias -Name "tfap" -Value New-TFAA
 
-function New-TFsp { terraform state pull }
+function New-TFsl { terraform state list > statelist.txt}
+set-alias -Name "tfsl" -Value New-TFsp
+
+function New-TFsp { terraform state pull > statepull.txt}
 set-alias -Name "tfsp" -Value New-TFsp
 
 function New-TFF { terraform fmt -recursive }
--- Random/TFEWorkspaces.ps1
+++ Random/TFEWorkspaces.ps1
@@ -2,12 +2,13 @@
 
 $tfeToken = ""
 
+$org = "technology-core"
 $Headers = @{
     Authorization  = "Bearer $tfeToken"
     "Content-Type" = "application/vnd.api+json"
 }
 
-$baseUrl = "https://app.terraform.io/api/v2/organizations/cudirect-namespace/workspaces"
+$baseUrl = "https://app.terraform.io/api/v2/organizations/$org/workspaces"
 
 
 
@@ -19,12 +20,3 @@ $body = @{
 $response = Invoke-RestMethod -Uri $baseUrl -Headers $Headers -Body $body
 $allWorkspaces = $response.data | select id, $_.attributes.name;
 $allWorkspaces
-
-foreach ($workspace in $allWorkspaces){
-
-    $newurl = -join($baseUrl, "/", $workspace.id)
-    $newurl
-    Invoke-RestMethod -Uri $newurl -Headers $header
-}
-
-# ($response.data | select id).count
